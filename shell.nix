b5c5fc3fc652883fc355f212a883eac08e098e38
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-08-18 00:11:03 -0700

    Fix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     clang # C++ compiler
     cmake # CMake build system
     cmake-format # CMake format tool
-    nixfmt-classic # Nix formatter
+    nixfmt # Nix formatter
+    nixfmt-tree # Nix formatter tree
     just # Just runner
   ];
 
